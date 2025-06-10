b088f40da7bedef05f4affa587c9bed8153be3c1
alishasofeaalii <alisha.sofea.ali@student.mmu.edu.my>
2025-06-10 23:59:14 +0800

    10/6

---
+++ PLANWISE-7-10-main/myProject/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include  # include is important
+from django.urls import path
+from myApp.views import music_player
+
+urlpatterns = [
+    path('admin/', admin.site.urls, name='admin'),
+    path('', include('myApp.urls')),
+    path('music/', music_player, name='music_player'),
+]
